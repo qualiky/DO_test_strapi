688a60b7764a48b8cadf47d9f41b5842cc2216fe
Sandeep Gautam <sandeepgautam568@gmail.com>
2024-07-19 08:35:27 +0545

    initial commit

---
+++ src/api/myApi/routes/myApi.js
@@ -0,0 +1,13 @@
+module.exports = {
+  routes: [
+    // {
+    //  method: 'GET',
+    //  path: '/myApi',
+    //  handler: 'myApi.exampleAction',
+    //  config: {
+    //    policies: [],
+    //    middlewares: [],
+    //  },
+    // },
+  ],
+};
